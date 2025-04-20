2ebf1c78bc1a7fd9a679d2025ca7409437add81b
sciencenity <gandown1@gmail.com>
2025-04-20 20:33:28 +0300

    Added Key Events

---
+++ rot13v1/mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+QString MainWindow::rot(std::string str){
+    std::string resp;
+        for (size_t i = 0; i < str.size(); ++i) {
+            if (isalpha(str[i])) {
+                if ((tolower(str[i]) - 'a') < 13)
+                    resp.append(1, str[i] + 13);
+                else
+                    resp.append(1, str[i] - 13);
+            } else {
+                resp.append(1, str[i]);
+            }
+        }
+        return  QString::fromStdString(resp);
+}
+void MainWindow::on_pushButton_clicked()
+{
+    QString toEncode = ui->lineEdit->text();
+    std::string str = toEncode.toStdString();
+    ui->textBrowser->setText(rot(str));
+}
+void MainWindow::keyPressEvent(QKeyEvent *e){
+    if(e->key() == Qt::Key_Control){
+        QString toEncode = ui->lineEdit->text();
+        std::string str = toEncode.toStdString();
+        ui->textBrowser->setText(rot(str));
+    }
+}
